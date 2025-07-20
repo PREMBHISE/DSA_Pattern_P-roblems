98281cbed8b38ded558f61f968760ade4c8d339f
PREMBHISE <166536696+PREMBHISE@users.noreply.github.com>
2025-07-20 23:43:08 +0530

    Add files via upload

--- HalfPyramid.java
+++ HalfPyramid.java
@@ -1,11 +1,27 @@
 public class HalfPyramid {
     public static void main (String[] args) {
-        int n= 4;
+        int n = 5;
 
-        for (int i = 1; i<=n; i++){
-            for (int j = 1; j<=i; j++) {
-                System.out.print("*");
+        
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=n-i; j++) {
+                System.out.print(" ");
             }
+
+            for(int j = 1; j<=i; j++ ) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=i; j++ ) {
+                System.out.print(j);
+            }
+            for(int j = 1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
             System.out.println();
         }
     }
